c3d287d956304adbbd67893aa98dd68bc57992d0
Davi de Sena Eduardo <davidesenaeduardo@gmail.com>
2022-10-05 00:32:40 -0300

    POST imagens na API do IMGUR

--- contacomigo/src/main/java/br/com/contacomigo/service/HistoriaService.java
+++ contacomigo/src/main/java/br/com/contacomigo/service/HistoriaService.java
@@ -2,12 +2,17 @@ package br.com.contacomigo.service;
 
 import br.com.contacomigo.model.HistoriaModel;
 import br.com.contacomigo.repository.HistoriaRepository;
+import okhttp3.*;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +22,8 @@ import java.util.UUID;
 @Service
 public class HistoriaService {
 
+
+
     @Autowired private HistoriaRepository repository;
 
     private static String caminhoImagens = "C:/Users/Davi/Desktop/contacomigo-backend/imagens/";
@@ -24,6 +31,12 @@ public class HistoriaService {
     private String extrairExtensao(String nomeImagem){
         int i = nomeImagem.lastIndexOf(".");
         return nomeImagem.substring(i+1);
+    }
+
+    public  static File multipartToFile(MultipartFile multipart, String fileName) throws IllegalStateException, IOException {
+        File convFile = new File(System.getProperty("java.io.tmpdir")+"/"+fileName);
+        multipart.transferTo(convFile);
+        return convFile;
     }
 
     public List<HistoriaModel> getAllHistorias(){
@@ -57,15 +70,33 @@ public class HistoriaService {
         historiaModel.setSubCategoria(subCategoria);
         try{
             for (int i = 0; i < imagens.length; i++ ){
-                var novoNome = UUID.randomUUID() + "." + extrairExtensao(imagens[i].getOriginalFilename());
-                var caminhoFinal = caminhoImagens + novoNome;
-                Files.copy(imagens[i].getInputStream(), Path.of(caminhoFinal), StandardCopyOption.REPLACE_EXISTING);
-                historiaModel.setImgsHistoria(novoNome, i);
+                OkHttpClient client = new OkHttpClient().newBuilder()
+                        .build();
+                MediaType mediaType = MediaType.parse("text/plain");
+                RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                        .addFormDataPart("image",imagens[i].getOriginalFilename(),
+                                RequestBody.create(MediaType.parse("application/octet-stream"),
+                                        new File(multipartToFile(imagens[i], "temporario").toURI())))
+                        .build();
+                Request request = new Request.Builder()
+                        .url("https://api.imgur.com/3/upload")
+                        .method("POST", body)
+                        .addHeader("Authorization", "Bearer 5eeae49394cd929e299785c8805bd168fc675280")
+                        .build();
+                Response response = client.newCall(request).execute();
+                System.out.println(response);
+                JSONObject imgur = new JSONObject(response.body().string());
+                JSONObject data = imgur.getJSONObject("data");
+                String imgLink = (String) data.getString("link");
+
+                historiaModel.setImgsHistoria(imgLink, i);
             }
+            return this.repository.save(historiaModel);
+
         }catch (Exception e){
             System.out.println(e);
+            return null;
         }
-        return this.repository.save(historiaModel);
     }
 
     public HistoriaModel deletarHistoria(long id){
