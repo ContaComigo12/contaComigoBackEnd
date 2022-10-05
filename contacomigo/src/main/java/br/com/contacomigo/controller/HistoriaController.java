62153eb36aeb4ea5ef4e399531be34cb92f46f62
Davi de Sena Eduardo <davidesenaeduardo@gmail.com>
2022-10-05 19:51:55 -0300

    corrigindo Mapping

--- contacomigo/src/main/java/br/com/contacomigo/controller/HistoriaController.java
+++ contacomigo/src/main/java/br/com/contacomigo/controller/HistoriaController.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Controller
-@RequestMapping({"/historia/historia"})
+@RequestMapping({"/historia"})
 @CrossOrigin({"*"})
 public class HistoriaController {
 
